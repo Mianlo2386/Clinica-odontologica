ce7a1d723853dc3ba4fe3516042551d8eedf5405
Miguel Lopez <miguellopez32@gmail.com>
2024-11-12 23:04:19 -0300

    Inicio de clinica odontologica

---
+++ target/generated-sources/annotations/logica/Responsable_.java
@@ -0,0 +1,13 @@
+package logica;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2024-11-12T22:55:52")
+@StaticMetamodel(Responsable.class)
+public class Responsable_ extends Persona_ {
+
+    public static volatile SingularAttribute<Responsable, String> tipo_resp;
+
+}
---
+++ target/generated-sources/annotations/logica/Secretario_.java
@@ -0,0 +1,15 @@
+package logica;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import logica.Usuario;
+
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2024-11-12T22:55:52")
+@StaticMetamodel(Secretario.class)
+public class Secretario_ extends Persona_ {
+
+    public static volatile SingularAttribute<Secretario, Usuario> unUsuario;
+    public static volatile SingularAttribute<Secretario, String> sector;
+
+}
